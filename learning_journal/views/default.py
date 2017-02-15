b62610f3166a209b79c717fae611d5f90ff55e67
Brad Baumel <Baumelbi@gmail.com>
2017-02-14 19:45:02 -0800

    everything working through the break in class

--- learning_journal/views/default.py
+++ learning_journal/views/default.py
@@ -4,16 +4,42 @@ from pyramid.view import view_config
 from sqlalchemy.exc import DBAPIError
 
 from ..models import Entry
+from ..models.mymodel import Entry, DBSession
+from pyramid.httpexceptions import HTTPNotFound
 
 
-@view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
-def my_view(request):
-    try:
-        query = request.dbsession.query(MyModel)
-        one = query.filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(db_err_msg, content_type='text/plain', status=500)
-    return {'one': one, 'project': 'learning_journal'}
+# @view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
+# def my_view(request):
+#     try:
+#         query = request.dbsession.query(MyModel)
+#         one = query.filter(MyModel.name == 'one').first()
+#     except DBAPIError:
+#         return Response(db_err_msg, content_type='text/plain', status=500)
+#     return {'one': one, 'project': 'learning_journal'}
+
+@view_config(route_name='home', renderer='string')
+def index_page(request):
+    entries = Entry.all()
+    return {"entries:entries"}
+
+
+@view_config(route_name='detail', renderer='string')
+def view(request):
+    this_id = request.matchdict.get('id', -1)
+    entry = Entry.by_id(this_id)
+    if not entry:
+        return HTTPNotFound()
+    return {'entry': entry}
+
+
+@view_config(route_name='action', match_param='action=create', renderer='string')
+def create(request):
+    return 'create page'
+
+
+@view_config(route_name='action', match_param='action=edit', renderer='string')
+def update(request):
+    return 'edit page'
 
 
 db_err_msg = """\
